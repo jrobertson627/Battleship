83f73868629d6b3e009cf5a4a1c3dc896d40ad89
jrobertson627 <jrobertson2@zagmail.gonzaga.edu>
2021-01-17 14:46:46 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BattleShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We're going to code a game of battleship!");
+            //Function to create a blank field (8x8)
+            //Function to allow the user to pick where they want to go
+            //Function to generate a random placement of ships
+            //Function for a turn
+                //User should be able to hit or miss
+                //mark it on a map for them
+            //determine when the game is over
+
+            //extensions
+            //two player game instead of player vs com
+            //?
+
+
+        }
+    }
+}
