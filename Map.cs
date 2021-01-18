e73b464195f80deb4353ab3c503d487d3e50f5a1
jrobertson627 <jrobertson2@zagmail.gonzaga.edu>
2021-01-17 21:30:15 -0800

    Stubbed out the Map Object

---
+++ Map.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Map
+{
+	public Map()
+	{
+		createBlankMap();
+	}
+
+	
+	//create a blank map with 0-7 and A-H for coordinates
+	public void CreateBlankMap()
+    {
+		//create the 0-7 line up top
+		//create the A-H line and follow with *
+    }
+
+	//map getter
+	public Map GetMap()
+    {
+		return null;
+    }
+
+	//allow the user to change the map
+	public void ChangeMap()
+    {
+
+    }
+}
